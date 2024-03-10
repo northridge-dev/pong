f04e4df267e4be5239b22a8fae9dc40bcf247c18
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-03-10 10:02:26 -0500

    refactor, extend starter code

--- pong.py
+++ pong.py
@@ -1,28 +1,88 @@
 import turtle
 
+"""
+Global constants
+"""
+# screen dimensions, borders
+screen_width = 800
+screen_height = 600
+right_edge = screen_width / 2
+left_edge = -right_edge
+top_edge = screen_height / 2
+bottom_edge = -top_edge
+middle = 0
+
+
+"""
+Game State
+"""
+score_a = 0
+score_b = 0
+winning_score = 5
+higher_score = max(score_a, score_b)
+
+"""
+Window
+"""
+window = turtle.Screen()
+window.title("Knight's Pong")
+window.bgcolor("black")
+window.setup(width=screen_width, height=screen_height)
+window.tracer(0)  # control screen updates with game loop
+
+""" 
+Ball
+"""
+# add code here to initialize the ball
+
+""" 
+Paddle A
+"""
+# add code here to initialize paddle A
+
+""" 
+Paddle B
+"""
+# add code here to initialize paddle B
+
+
+""" 
+Scoreboard
+"""
+# add code here to initialize the scoreboard
+
+"""
+Start Message 
+"""
+start_message = turtle.Turtle()
+start_message.color("white")
+start_message.write(
+    "Press space to start", align="center", font=("Courier", 24, "normal")
+)
+
+"""
+Helper Functions
+"""
+
+
+"""
+Main Game Loop
+"""
+
 
 def play_pong():
-    # screen dimensions, borders
-    screen_width = 800
-    right_edge = screen_width / 2
-    left_edge = -right_edge
-
-    screen_height = 600
-    top_edge = screen_height / 2
-    bottom_edge = -top_edge
-
-    middle = 0
-
-    # initialize screen
-    screen = turtle.Screen()
-    screen.title("Knight's Pong")
-    screen.bgcolor("black")
-    screen.setup(width=screen_width, height=screen_height)
-    screen.tracer(0)  # control screen updates with game loop
+    start_message.clear()  # clear start message
 
     # main game loop
-    while True:
-        screen.update()  # redraws screen on each tick
+    while higher_score < winning_score:
+        window.update()  # redraws screen on each tick
 
 
-play_pong()
+"""
+Event listeners
+"""
+window.onkey(play_pong, "space")
+window.listen()
+
+
+turtle.mainloop()
